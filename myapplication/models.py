498b21fe0111af7a546b95c718dd56a8b3f9bc1f
weskaroki <weskaroki@gmail.com>
2024-03-13 16:42:56 +0300

    initial commit

---
+++ myapplication/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Employees(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=15)
+    salary = models.IntegerField()
+    def __str__(self):
+        return self.name
+
